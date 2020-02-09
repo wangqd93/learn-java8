2bed476f3987cb933d735f45e4d183220709f7fb
wangqidong <wangqidong@shuidihuzhu.com>
2020-02-09 14:39:26 +0800

    add flatMap test

---
+++ src/main/java/com/bycsmys/character5/FlatMapTest.java
@@ -0,0 +1,31 @@
+package com.bycsmys.character5;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * @Author wangqd
+ * @DATE 2020-02-09
+ */
+public class FlatMapTest {
+
+    public static void main(String[] args) {
+
+        // ["hello","world"] -> ["h","e","l","o","w","r","d"]
+        Stream.of("hello", "world").map(s -> s.split(""))
+                .flatMap(Stream::of).distinct()
+                .forEach(System.out::println);
+
+        System.out.println("--------------------");
+
+        //[1,2,3] [3,4] -> [(1,3),(1,4),(2,3),(2,4),(3,3),(3,4)]
+        List<Integer> list = Arrays.asList(1, 2, 3);
+        List<Integer> list2 = Arrays.asList(3, 4);
+        list.stream()
+                .flatMap(l -> list2.stream().map(l2 -> "(" + l + "," + l2 + ")"))
+                .forEach(System.out::println);
+
+
+    }
+}
